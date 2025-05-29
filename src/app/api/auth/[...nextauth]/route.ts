2955183792a467a0a2d02320ba62f37b64388342
Aetherx24 <iqbalfahrur@gmail.com>
2025-05-30 02:09:51 +0700

    feat: added middleware route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        try {
+          const res = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          });
+
+          const user = await res.json();
+
+          if (res.ok && user) {
+            return user;
+          }
+          return null;
+        } catch (error) {
+          return null;
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+})
+
+export { handler as GET, handler as POST } 

1c1fc4b47b7a0ccd14c5f2ca09cb6fc3935a6309
Aetherx24 <iqbalfahrur@gmail.com>
2025-05-30 23:37:01 +0700

    feat: testing and setup

---
+++ jest.setup.js
@@ -0,0 +1,35 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
+
+// Mock next/router
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: {},
+      asPath: '',
+      push: jest.fn(),
+      replace: jest.fn(),
+    }
+  },
+}))
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />
+  },
+}))
+
+// Mock next-auth
+jest.mock('next-auth/react', () => ({
+  useSession: () => ({
+    data: null,
+    status: 'unauthenticated',
+  }),
+  signIn: jest.fn(),
+  signOut: jest.fn(),
+})) 
